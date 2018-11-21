1a3c1ec31128eba7964e507f2e64a108139d658a
MD Tareq Mahmud Khan <43804909+tareq1047@users.noreply.github.com>
2018-11-21 11:01:07 +0600

    Modal1 edited in index

--- Index folder/index.php
+++ Index folder/index.php
@@ -117,3 +117,21 @@
 			</div>
 	</div>	
 
+     
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog">
+						<!-- Modal1 -->
+							<div class="modal-dialog">
+							<!-- Modal content-->
+								<div class="modal-content">
+									<div class="modal-header">
+										<button type="button" class="close" data-dismiss="modal">&times;</button>
+										<h4>Red roff <span>Inn</span></h4>
+										<img src="images/1.jpg" alt=" " class="img-responsive">
+										<h5>We know what you love</h5>
+										<p>Providing guests unique and enchanting views from their rooms with its exceptional amenities, makes Star Hotel one of bests in its kind.Try our food menu, awesome services and friendly staff while you are here.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+
+
