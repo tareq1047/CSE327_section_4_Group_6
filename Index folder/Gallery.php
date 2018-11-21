05f3ac1673e3b938e6a96c50de91282032cbb040
MD Tareq Mahmud Khan <43804909+tareq1047@users.noreply.github.com>
2018-11-22 01:41:53 +0600

    Gallery section for index page

---
+++ Index folder/Gallery.php
@@ -0,0 +1,100 @@
+<section class="portfolio-w3ls" id="gallery">
+		 <h3 class="title-w3-agileits title-black-wthree">Our Gallery</h3>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g1.jpg" class="swipebox"><img src="images/g1.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Red roof inn</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+						</div>
+				</a>
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g2.jpg" class="swipebox"><img src="images/g2.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Red Roff</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+						</div>
+				</a>
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g3.jpg" class="swipebox"><img src="images/g3.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Red Roof</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+						</div>
+				</a>
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g4.jpg" class="swipebox"><img src="images/g4.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Red Roof</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+						</div>
+				</a>
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g5.jpg" class="swipebox"><img src="images/g5.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Red Roof</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g6.jpg" class="swipebox"><img src="images/g6.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Red Roof</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+					   </div>
+				   </a>
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g6.jpg" class="swipebox"><img src="images/g7.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Red Roof</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+					   </div>
+				   </a>
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g6.jpg" class="swipebox"><img src="images/g8.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Red Roof</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+					   </div>
+				   </a>
+				</div>
+					<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g9.jpg" class="swipebox"><img src="images/g9.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Red Roof</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+						</div>
+				</a>
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g10.jpg" class="swipebox"><img src="images/g10.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Red Roof</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+						</div>
+				</a>
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g4.jpg" class="swipebox"><img src="images/g4.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Red Roof</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+						</div>
+				</a>
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g2.jpg" class="swipebox"><img src="images/g2.jpg" class="img-responsive" alt="/">
+						<div class="textbox">
+						<h4>Red Roof</h4>
+							<p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+						</div>
+				</a>
+				</div>
+				<div class="clearfix"> </div>
+</section>
