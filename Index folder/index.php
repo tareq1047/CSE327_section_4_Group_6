ada719e5a8340aca25a46cde36ea1c355598c1cd
MD Tareq Mahmud Khan <43804909+tareq1047@users.noreply.github.com>
2018-11-22 00:09:33 +0600

    service section added in index

--- Index folder/index.php
+++ Index folder/index.php
@@ -135,3 +135,34 @@
 						</div>
 
 
+<div class="advantages">
+	<div class="container">
+		<div class="advantages-main">
+				<h3 class="title-w3-agileits">Our Services</h3>
+		   <div class="advantage-bottom">
+			 <div class="col-md-6 advantage-grid left-w3ls wow bounceInLeft" data-wow-delay="0.3s">
+			 	<div class="advantage-block ">
+					<i class="fa fa-credit-card" aria-hidden="true"></i>
+			 		<h4>Stay First, Pay After! </h4>
+			 		<p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates.</p>
+					<p><i class="fa fa-check" aria-hidden="true"></i>Decorated room, proper air conditioned</p>
+					<p><i class="fa fa-check" aria-hidden="true"></i>Private balcony</p>
+			 		
+			 	</div>
+			 </div>
+			 <div class="col-md-6 advantage-grid right-w3ls wow zoomIn" data-wow-delay="0.3s">
+			 	<div class="advantage-block">
+					<i class="fa fa-clock-o" aria-hidden="true"></i>
+			 		<h4>24 Hour Restaurant</h4>
+			 		<p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates.</p>
+					<p><i class="fa fa-check" aria-hidden="true"></i>24 hours room service</p>
+					<p><i class="fa fa-check" aria-hidden="true"></i>24-hour Concierge service</p>
+			 	</div>
+			 </div>
+			<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
