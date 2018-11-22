f13f1fcb45b26618dc3f838e60439e94a45f6378
MD Tareq Mahmud Khan <43804909+tareq1047@users.noreply.github.com>
2018-11-23 02:13:55 +0600

    index edited

--- Index folder/index.php
+++ Index folder/index.php
@@ -133,6 +133,12 @@
 								</div>
 							</div>
 						</div>
+<div id="availability-agileits">
+<div class="col-md-12 book-form-left-w3layouts">
+	<a href="admin/reservation.php"><h2>ROOM RESERVATION</h2></a>
+</div>
+
+			<div class="clearfix"> </div>
 
  
  	<div class="about-wthree" id="about">
