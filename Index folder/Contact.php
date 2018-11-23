8bfc24fd0b032b0893762f8652a625667e82d3b9
MD Tareq Mahmud Khan <43804909+tareq1047@users.noreply.github.com>
2018-11-24 01:08:25 +0600

    php function for form

--- Index folder/Contact.php
+++ Index folder/Contact.php
@@ -31,3 +31,19 @@
                     
                     <input type="submit" name="sub" value="Send Now" class="btn btn-primary">	
 				</form>
+				
+			<?php
+				if(isset($_POST['sub']))
+				{
+					$name =$_POST['name'];
+					$phone = $_POST['phone'];
+					$email = $_POST['email'];
+					$approval = "Not Allowed";
+					$sql = "INSERT INTO `contact`(`fullname`, `phoneno`, `email`,`cdate`,`approval`) VALUES ('$name','$phone','$email',now(),'$approval')" ;
+					
+					
+					if(mysqli_query($con,$sql))
+					echo"OK";
+					
+				}
+				?>
