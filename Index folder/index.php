e82792ad8bb6e433490b46afc1a5a16e9c101fd3
MD Tareq Mahmud Khan <43804909+tareq1047@users.noreply.github.com>
2018-11-24 02:34:31 +0600

    merge in main index page

--- Index folder/index.php
+++ Index folder/index.php
@@ -218,5 +218,73 @@
 		</div>
 	</div>
 </div>
+	
+	<!-- contact -->
+<section class="contact-w3ls" id="contact">
+	<div class="container">
+		<div class="col-lg-6 col-md-6 col-sm-6 contact-w3-agile2" data-aos="flip-left">
+			<div class="contact-agileits">
+				<h4>Contact Us</h4>
+				<p class="contact-agile2">Sign Up For Our News Letters</p>
+				<form  method="post" name="sentMessage" id="contactForm" >
+					<div class="control-group form-group">
+                        
+                            <label class="contact-p1">Full Name:</label>
+                            <input type="text" class="form-control" name="name" id="name" required >
+                            <p class="help-block"></p>
+                       
+                    </div>	
+                    <div class="control-group form-group">
+                        
+                            <label class="contact-p1">Phone Number:</label>
+                            <input type="tel" class="form-control" name="phone" id="phone" required >
+							<p class="help-block"></p>
+						
+                    </div>
+                    <div class="control-group form-group">
+                        
+                            <label class="contact-p1">Email Address:</label>
+                            <input type="email" class="form-control" name="email" id="email" required >
+							<p class="help-block"></p>
+						
+                    </div>
+                    
+                    
+                    <input type="submit" name="sub" value="Send Now" class="btn btn-primary">	
+				</form>
+				
+			<?php
+				if(isset($_POST['sub']))
+				{
+					$name =$_POST['name'];
+					$phone = $_POST['phone'];
+					$email = $_POST['email'];
+					$approval = "Not Allowed";
+					$sql = "INSERT INTO `contact`(`fullname`, `phoneno`, `email`,`cdate`,`approval`) VALUES ('$name','$phone','$email',now(),'$approval')" ;
+					
+					
+					if(mysqli_query($con,$sql))
+					echo"OK";
+		<div class="col-lg-6 col-md-6 col-sm-6 contact-w3-agile1" data-aos="flip-right">
+			<h4>Connect With Us</h4>
+			<p class="contact-agile1"><strong>Phone :</strong>+8801674979197</p>
+			<p class="contact-agile1"><strong>Email :</strong> <a href="mailto:name@example.com">INo@Redroofinn.COM</a></p>
+			<p class="contact-agile1"><strong>Address :</strong> jhawtala road,kandirpar,comilla, bangladesh</p>
+																
+			<div class="social-bnr-agileits footer-icons-agileinfo">
+				<ul class="social-icons3">
+								<li><a href="#" class="fa fa-facebook icon-border facebook"> </a></li>
+								<li><a href="#" class="fa fa-twitter icon-border twitter"> </a></li>
+								<li><a href="#" class="fa fa-google-plus icon-border googleplus"> </a></li> 
+								
+							</ul>
+			</div>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3074.7905052320443!2d-77.84987248482734!3d39.586871613613056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c9f6a80ccf0661%3A0x7210426c67abc40!2sVirginia+Welcome+Center%2FSafety+Rest+Area!5e0!3m2!1sen!2sin!4v1485760915662" ></iframe>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+					
+				
+
 
 
